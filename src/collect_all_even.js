25b89233cab4b4fc906ad541682deb8b4f7c38ba
fanfanqu <18829291202@163.com>
2016-05-30 17:47:26 +0800

    file

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,6 +2,12 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+ 
+  var array = [];
+  for (var i = 0;i <collection.length;i++ ){
+	  if (collection[i]%2===0){
+		array.push(collection[i]);	
+	  }
+  }return array;
 }
-
 module.exports = collect_all_even;
